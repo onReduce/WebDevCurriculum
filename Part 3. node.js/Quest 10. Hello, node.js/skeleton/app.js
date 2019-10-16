f50896a9695df2cb1d62c67c53b27ebafefe0d48
gylee <gylee@knowre.com>
2019-10-16 11:46:53 +0900

    [WebDevCurriculum] solve quest 10
    CLI 입력값에 따라 설정 변경하기

--- Part 3. node.js/Quest 10. Hello, node.js/skeleton/app.js
+++ Part 3. node.js/Quest 10. Hello, node.js/skeleton/app.js
@@ -1,3 +1,10 @@
-const num = process.argv[2];
+// const num = process.argv[2];
 
-console.log(require('./config' + num + '.js'));
+// console.log(require('./config' + num + '.js'));
+
+const num = Number.parseInt(process.argv[2]);
+let config = '';
+if (!Number.isNaN(num)) {
+	config = require('./config/variables.json').filter(x => x.name === `Config${num}`);
+}
+console.info(config);
